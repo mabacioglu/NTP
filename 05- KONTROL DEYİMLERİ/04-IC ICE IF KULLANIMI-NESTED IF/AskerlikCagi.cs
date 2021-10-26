1f8262f9867de1d1f9a85879a21e416edab180fd
MEHMET ABACIOĞLU <mabacioglu@gmail.com>
2021-10-26 09:16:29 +0300

    iç içe if kullanımı

---
+++ 05- KONTROL DEYİMLERİ/04-IC ICE IF KULLANIMI-NESTED IF/AskerlikCagi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_IC_ICE_IF_KULLANIMI_NESTED_IF
+{
+    public partial class AskerlikCagi : Form
+    {
+        public AskerlikCagi()
+        {
+            InitializeComponent();
+        }
+
+        private void AskerlikCagi_Load(object sender, EventArgs e)
+        {
+            for (int i = 15; i <= 85; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rdbKadin.Checked==true)
+            {
+                MessageBox.Show("Kadınlar askere gitmezler...");
+            }
+            else
+            {
+                int yas=int.Parse(comboBox1.Text);
+                if (yas<20)
+                {
+                    MessageBox.Show("Askerlik Çağına girmediniz...");
+                }
+                else if (yas>40)
+                {
+                    MessageBox.Show("Askerlik Çağından çıktınız..");
+                }
+                else
+                {
+                    MessageBox.Show("Askerlik Çağı içerisindesiniz..");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ 05- KONTROL DEYİMLERİ/04-IC ICE IF KULLANIMI-NESTED IF/KullaniciAdiParola.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_IC_ICE_IF_KULLANIMI_NESTED_IF
+{
+    public partial class KullaniciAdiParola : Form
+    {
+        public KullaniciAdiParola()
+        {
+            InitializeComponent();
+        }
+     
+       
+
+
+                private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+                private void button1_Click(object sender, EventArgs e)
+                {
+                    string kAdi, parola;
+                    kAdi = txtKullaniciAdi.Text;
+                    parola = txtParola.Text;
+                    if (kAdi=="admin")
+                    {
+                        if (parola=="1234")
+                        {
+                            MessageBox.Show("Giriş Başarılı");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Parola Hatalı!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Kullanıcı adı Hatalı!");
+                    }
+                }
+    }
+}
