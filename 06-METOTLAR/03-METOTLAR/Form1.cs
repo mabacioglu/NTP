939204ac9729783f44a38f55a770e58ba7c9339e
MEHMET ABACIOĞLU <mabacioglu@gmail.com>
2021-12-03 11:19:28 +0300

    metotlar

--- 06-METOTLAR/03-METOTLAR/Form1.cs
+++ 06-METOTLAR/03-METOTLAR/Form1.cs
@@ -16,7 +16,7 @@ namespace _03_METOTLAR
         {
             InitializeComponent();
         }
-
+        // MEHMET ABACIOĞLU- PARAMETRE ALAN DEĞER DÖNDERMEYEN -1 
         private void button1_Click(object sender, EventArgs e)
         {
             int sayi1 = int.Parse(txtSayi1.Text);
@@ -24,24 +24,38 @@ namespace _03_METOTLAR
 
             if (sayi1 > sayi2)
             {
-                int alan = sayi1 * sayi2;
-                int cevre = 2 * (sayi1 + sayi2);
-                MessageBox.Show("Dikdörtgenin Alanı= " + alan.ToString() + "  Çevresi= " + cevre.ToString());
+                Dikdortgen(sayi1, sayi2);
             }
             else if (sayi1 < sayi2)
             {
-                int alan = (sayi1 * sayi2) / 2;
-                int cevre = 2 * (sayi1 + sayi2);
-                MessageBox.Show("Üçgenin Alanı= " + alan.ToString() + "  Çevresi= " + cevre.ToString());
+                Ucgen(sayi1, sayi2);
             }
             else
             {
-                int alan = sayi1 * sayi1;
-                int cevre = 4 * sayi1 ;
-                MessageBox.Show("Karenen Alanı= " + alan.ToString() + "  Çevresi= " + cevre.ToString());
+                Kare(sayi1);
             }
                 
          }
+
+        private static void Kare(int sayi1)
+        {
+            int alan = sayi1 * sayi1;
+            int cevre = 4 * sayi1;
+            MessageBox.Show("Karenin Alanı= " + alan.ToString() + "  Çevresi= " + cevre.ToString());
+        }
+
+        private static void Ucgen(int sayi1, int sayi2)
+        {
+            int alan = (sayi1 * sayi2) / 2;
+            MessageBox.Show("Üçgenin Alanı= " + alan.ToString());
+        }
+
+        private static void Dikdortgen(int sayi1, int sayi2)
+        {
+            int alan = sayi1 * sayi2;
+            int cevre = 2 * (sayi1 + sayi2);
+            MessageBox.Show("Dikdörtgenin Alanı= " + alan.ToString() + "  Çevresi= " + cevre.ToString());
+        }
 
         
         
---
+++ 06-METOTLAR/03-METOTLAR/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03_METOTLAR
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = int.Parse(txtSayi1.Text);
+            int sayi2 = int.Parse(txtSayi2.Text);
+
+            int sonuc=Topla(sayi1, sayi2);
+            label1.Text = sonuc.ToString();
+
+        }
+
+        private int Topla(int x, int y)
+        {
+            if (x>y)
+                return x-y;
+            else
+                return y-x;
+        
+        }
+    }
+}
