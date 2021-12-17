364423c119712f435712d706c093baae58e9e341
MEHMET ABACIOĞLU <mabacioglu@gmail.com>
2021-12-17 12:04:31 +0300

    break-continue-Sayı Oyunu

---
+++ 07-DONGULER/07-DONGULER/Form8.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace _07_DONGULER
+{
+    partial class Form8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOyuncu1 = new System.Windows.Forms.TextBox();
+            this.txtOyuncu2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblOyuncu2 = new System.Windows.Forms.Label();
+            this.lblOyuncu1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOyuncu1
+            // 
+            this.txtOyuncu1.Location = new System.Drawing.Point(139, 49);
+            this.txtOyuncu1.Name = "txtOyuncu1";
+            this.txtOyuncu1.Size = new System.Drawing.Size(57, 20);
+            this.txtOyuncu1.TabIndex = 0;
+            // 
+            // txtOyuncu2
+            // 
+            this.txtOyuncu2.Location = new System.Drawing.Point(244, 49);
+            this.txtOyuncu2.Name = "txtOyuncu2";
+            this.txtOyuncu2.Size = new System.Drawing.Size(57, 20);
+            this.txtOyuncu2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Oyuncu1 Tahmin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Oyuncu2 Tahmin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(191, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(121, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(178, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "PUANLAR";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(121, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Oyuncu 1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(215, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Oyuncu 2";
+            // 
+            // lblOyuncu2
+            // 
+            this.lblOyuncu2.AutoSize = true;
+            this.lblOyuncu2.Location = new System.Drawing.Point(235, 197);
+            this.lblOyuncu2.Name = "lblOyuncu2";
+            this.lblOyuncu2.Size = new System.Drawing.Size(13, 13);
+            this.lblOyuncu2.TabIndex = 2;
+            this.lblOyuncu2.Text = "0";
+            // 
+            // lblOyuncu1
+            // 
+            this.lblOyuncu1.AutoSize = true;
+            this.lblOyuncu1.Location = new System.Drawing.Point(144, 197);
+            this.lblOyuncu1.Name = "lblOyuncu1";
+            this.lblOyuncu1.Size = new System.Drawing.Size(13, 13);
+            this.lblOyuncu1.TabIndex = 2;
+            this.lblOyuncu1.Text = "0";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(74, 212);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OYNA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblOyuncu1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblOyuncu2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOyuncu2);
+            this.Controls.Add(this.txtOyuncu1);
+            this.Name = "Form8";
+            this.Text = "Form8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOyuncu1;
+        private System.Windows.Forms.TextBox txtOyuncu2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblOyuncu2;
+        private System.Windows.Forms.Label lblOyuncu1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ 07-DONGULER/07-DONGULER/Form8.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_DONGULER
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+        int puan1 = 0, puan2 = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rnd = new Random();
+            int sayi,sayi1, sayi2;
+            sayi1 = int.Parse(txtOyuncu1.Text);
+            sayi2 = int.Parse(txtOyuncu2.Text);
+            while (true)
+            {
+                sayi = rnd.Next(1, 51);
+                listBox1.Items.Add(sayi);
+                if (sayi1 == sayi)
+                {
+                    MessageBox.Show("1.oyuncu");
+                    puan1++;
+                    lblOyuncu1.Text = puan1.ToString();
+                    break;
+                }
+                if (sayi2 == sayi)
+                {
+                    MessageBox.Show("2.oyuncu");
+                    puan2++;
+                    lblOyuncu2.Text = puan2.ToString();
+                    break; 
+                }
+            }
+            if (puan2 == 5 || puan1 == 5)
+            {
+                string kazanan;
+                if (puan1 == 5)
+                    kazanan = "Oyuncu 1";
+                else
+                    kazanan = "Oyuncu 2";
+                MessageBox.Show("Oyunu "+kazanan+ " kazanmıştır");
+                Application.Exit();
+            }
+        }
+    }
+}
