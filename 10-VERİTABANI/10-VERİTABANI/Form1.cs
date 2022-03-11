b02415894559e1c067f63c1902e3bbb0f4885b40
MEHMET ABACIOĞLU <mabacioglu@gmail.com>
2022-03-11 10:25:44 +0300

    veritabanı

---
+++ 10-VERİTABANI/10-VERİTABANI/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace _10_VERİTABANI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection con;
+        OleDbDataAdapter da;
+        DataTable dt;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=okul.accdb");
+            da = new OleDbDataAdapter("select * from Bolum",con);
+            dt = new DataTable();
+            con.Open();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+    }
+}
